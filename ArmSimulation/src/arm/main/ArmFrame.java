6fc64b80fd79b0e47aeddb6cf1b3be5b3d63d635
Komota <Komota@Kaneko-PC>
2015-08-07 15:31:25 +0900

    ArmFrameの表示、MyVectorの作成 他

--- ArmSimulation/src/arm/main/ArmFrame.java
+++ ArmSimulation/src/arm/main/ArmFrame.java
@@ -1,6 +1,14 @@
 package arm.main;
 
-public class ArmFrame {
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JFrame;
+
+public class ArmFrame extends JFrame{
 
 	/**
 	 * @param args
@@ -9,6 +17,59 @@ public class ArmFrame {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello World from dynabook!");
 		System.out.println("Hello World from Prime!");
+
+		ArmFrame frame = new ArmFrame();
 	}
 
+	//定数
+	final Color BGCOLOR = new Color(245,245,245);
+
+	//バッファストラテジ
+	BufferStrategy buffer;
+
+	//コンストラクタ
+	public ArmFrame(){
+		this.setTitle("ArmFrame");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(1000,700);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+
+		this.setVisible(true);
+
+		this.createBufferStrategy(2);
+		this.setIgnoreRepaint(true);
+		this.buffer = this.getBufferStrategy();
+
+		Timer t = new Timer();
+		t.schedule(new RenderTask(), 0,16);
+	}
+
+
+	void render(){
+		System.out.println("render");
+		this.drawBackGround();
+		this.buffer.show();
+	}
+
+	//背景の描画。これは描画店の移動を行う前に実行する
+	void drawBackGround(){
+		Graphics2D g = (Graphics2D)this.buffer.getDrawGraphics();
+		g.setColor(this.BGCOLOR);
+		g.fillRect(0,0,this.getWidth(),this.getHeight());
+		g.dispose();
+	}
+
+
+	//TimerTask
+
+	class RenderTask extends TimerTask{
+
+		@Override
+		public void run() {
+			// TODO 自動生成されたメソッド・スタブ
+			ArmFrame.this.render();
+		}
+
+	}
 }
--- ArmSimulation/src/arm/main/MyObject.java
+++ ArmSimulation/src/arm/main/MyObject.java
@@ -1,5 +1,15 @@
 package arm.main;
 
+import java.awt.geom.Point2D;
+
 public class MyObject {
+
+	//Handの関節含め、ArmFrame上に存在する「点物体」のクラス
+
+	//フィールド
+
+	//位置ベクトル
+	Point2D.Double position;
+	
 
 }
