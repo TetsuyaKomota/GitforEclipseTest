c55169fb14349d4c276bc4fe69cadee068bed15c
Komota <komotatit@gmail.com>
2015-08-22 17:36:19 +0900

    ログの取り方修正せんとなー。

---
+++ IntentionLearning/src/komota/main/SampleTask3.java
@@ -0,0 +1,105 @@
+package komota.main;
+
+public class SampleTask3 extends MainFrame{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		SampleTask3 task = new SampleTask3();
+	}
+
+	//コンストラクタ
+	public SampleTask3(){
+		super();
+		this.tasktitle = "Move the RED left.";
+		setOutputFile("test3.txt");
+		initialize();
+	}
+
+	@Override
+	public void initialize(){
+/*
+		this.panels[0].setStatus(0);
+		this.panels[1].setStatus(2);
+		this.panels[2].setStatus(0);
+		this.panels[3].setStatus(0);
+		this.panels[4].setStatus(0);
+		this.panels[5].setStatus(0);
+		this.panels[6].setStatus(0);
+		this.panels[7].setStatus(0);
+		this.panels[8].setStatus(1);
+*/
+		for(int i=0;i<this.panels.length;i++){
+			this.panels[i].setStatus(0);
+		}
+//		this.panels[(int)(Math.random()*3)].setStatus(2);
+//		this.panels[3+(int)(Math.random()*6)].setStatus(1);
+
+/*
+		int rand = (int)(Math.random()*6);
+
+		switch(rand){
+		case 0:
+			this.panels[0].setStatus(3);
+			this.panels[2].setStatus(3);
+			this.panels[7].setStatus(1);
+			this.panels[8].setStatus(6);
+			break;
+		case 1:
+			this.panels[3].setStatus(3);
+			this.panels[5].setStatus(3);
+			this.panels[0].setStatus(1);
+			this.panels[8].setStatus(6);
+			break;
+		case 2:
+			this.panels[6].setStatus(3);
+			this.panels[8].setStatus(3);
+			this.panels[1].setStatus(1);
+			this.panels[4].setStatus(6);
+			break;
+		case 3:
+			this.panels[0].setStatus(3);
+			this.panels[6].setStatus(3);
+			this.panels[1].setStatus(1);
+			this.panels[3].setStatus(6);
+			break;
+		case 4:
+			this.panels[1].setStatus(3);
+			this.panels[7].setStatus(3);
+			this.panels[8].setStatus(1);
+			this.panels[3].setStatus(6);
+			break;
+		case 5:
+			this.panels[2].setStatus(3);
+			this.panels[8].setStatus(3);
+			this.panels[0].setStatus(1);
+			this.panels[6].setStatus(6);
+			break;
+		}
+*/
+
+		int rand = (int)(Math.random() * 9);
+		this.panels[rand].setStatus(1);
+
+		this.outputStart();
+	}
+
+	/*
+	@Override
+	public void pushGoal(){
+		initialize();
+		pw.println("Done");
+	}
+	*/
+
+
+	/*
+	 *
+	 * １．出力先ファイル名をfile_nameに代入
+	 * ２．タスク名をtasktitleに代入
+	 * ３．initializeをオーバーライド。最終行にoutputStart()
+	 *
+	 */
+}
--- IntentionLearning/src/komota/pr/TestPatternRecognition.java
+++ IntentionLearning/src/komota/pr/TestPatternRecognition.java
@@ -1,12 +1,127 @@
 package komota.pr;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TestPatternRecognition {
+
+	//定数
+	//絶対移動の閾値
+	final double THRESHOLD = 0.9;
+	//絶対移動ベクトルの学習率
+	final double rate = 0.3;
+
+	//フィールド
+	File file;
+	BufferedReader br;
+	String file_name = "test3.txt";
+	//絶対移動ベクトル
+	double[] vec;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+
+		TestPatternRecognition test = new TestPatternRecognition();
+		test.testOutput();
+		test.close();
+
+	}
+
+
+	//コンストラクタ
+	public TestPatternRecognition(){
+		this.file = new File("log/"+file_name);
+		try {
+			this.br = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		this.vec = new double[2];
+		this.vec[0] = 0;
+		this.vec[1] = 1;
+	}
+
+	//クローズ処理
+	public void close(){
+		try {
+			this.br.close();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+	}
+
+	//とりあえず、最終行まで出力するテスト
+	public void testOutput(){
+		while(true){
+			String line = null;
+			try {
+				line = br.readLine();
+			} catch (IOException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+			if(line != null){
+				System.out.println(line);
+				updateVec(line);
+			}
+			else{
+				break;
+			}
+		}
+	}
+	//データをもとに、絶対移動する方向を推測する。
+	/*
+	 * テスト用なので、前提として
+	 * 1. 赤を動かすことを知っている
+	 * 2. 絶対移動であることを知っている
+	 * 3. ３マス平方の離散空間である
+	 */
+	public String[] updateVec(String input){
+
+		String regex_start = "\\*\\*start:";
+		String regex_goal = "\\*\\*goal:";
+		String regex_change = "  change:";
+		String regex_status = "status:";
+
+		Pattern pt_start = Pattern.compile(regex_start);
+		Pattern pt_goal = Pattern.compile(regex_goal);
+		Pattern pt_change = Pattern.compile(regex_change);
+		Pattern pt_status = Pattern.compile(regex_status);
+
+		Matcher m_start = pt_start.matcher(input);
+		Matcher m_goal = pt_goal.matcher(input);
+		Matcher m_change = pt_change.matcher(input);
+		Matcher m_status = pt_status.matcher(input);
+
+
+		//「  change:s,t」という文字列から「s」と「t」を取得してtemp2に格納する
+		if(m_change.find()){
+			System.out.println("Matched.");
+			String temp = input.split(regex_change)[1];
+			String[] temp2 = temp.split(",");
+			System.out.println("picked out "+temp2[0] + " and "+ temp2[1]);
+			return temp2;
+		}
+		//次の行の「status:p,p,p,p,p,p,p,p,p」という文字列からtemp3に状態を格納する。
+		else if(m_status.find()){
+			String temp = input.split(regex_status)[1];
+			String[] temp2 = temp.split(",");
+			System.out.println("picked out "+temp2[0] + " and "+ temp2[1]);
+			return temp2;
+		}
+		//temp2のどっちからどっちに、temp3の「１」が移動しているかを取得し、移動後の「１」の位置をtemp4に格納する
+		//temp2とtemp4からベクトルを取得し、学習率をかけてvecを更新する
+		return null;
 
 	}
 
