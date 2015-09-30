158c81c7e5b4ba073fdf75d4c2d6c142f1a63d8b
Komota <komotatit@gmail.com>
2015-09-30 13:17:05 +0900

    HMMの基本システムを構築

--- HMM/src/komota/main/testMain.java
+++ HMM/src/komota/main/testMain.java
@@ -1,14 +1,79 @@
 package komota.main;
 
+import komota.test.TestHMM;
+
 public class testMain {
 
 	/**
 	 * @param args
 	 */
+
+	//出力の定数
+	public static final int SUNNY = 0;
+	public static final int CLOUDY = 1;
+	public static final int RAINNY = 2;
+
+	//行動の定数
+	public final int WALKING = 0;
+	public final int WASHING = 1;
+	public final int READING = 2;
+
+
+
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
 		System.out.println("Hello HMM!");
+
+		TestHMM hmm = new TestHMM(3, 3);
+		//初期状態設定
+		double[] inits = {0.8,0.15,0.05};
+		hmm.setProInitial(inits);
+		//状態遷移確率設定
+		double[][] trans = new double[3][3];
+		trans[0][0] = 0.8;
+		trans[0][1] = 0.2;
+		trans[0][2] = 0.0;
+		trans[1][0] = 0.0;
+		trans[1][1] = 0.2;
+		trans[1][2] = 0.8;
+		trans[2][0] = 0.8;
+		trans[2][1] = 0.2;
+		trans[2][2] = 0.0;
+		hmm.setProTransition(trans);
+
+		//初期状態確率のテスト
+		System.out.println("Test for initialize");
+		for(int i=0;i<20;i++){
+			for(int j=0;j<20;j++){
+				hmm.initialize();
+				System.out.print(hmm.getCurStatus()+" ");
+			}
+			System.out.println("");
+		}
+
+		//出力確率のテスト
+		System.out.println("Test for output");
+		hmm.initialize();
+		System.out.println(hmm.getCurStatus()+" ");
+		for(int i=0;i<20;i++){
+			for(int j=0;j<20;j++){
+				System.out.print(hmm.output()+" ");
+			}
+			System.out.println("");
+		}
+
+		//状態遷移確率のテスト
+		System.out.println("Test for transition");
+		hmm.initialize();
+		System.out.println(hmm.getCurStatus()+" ");
+		for(int i=0;i<20;i++){
+			for(int j=0;j<20;j++){
+				hmm.transition();
+				System.out.print(hmm.getCurStatus()+" ");
+			}
+			System.out.println("");
+		}
 
 	}
 
--- HMM/src/komota/test/TestHMM.java
+++ HMM/src/komota/test/TestHMM.java
@@ -12,16 +12,14 @@ public class TestHMM {
 	int numstatus = 0;
 	//状態
 	int[] status = null;
-	//行動数
-	int numaction = 0;
-	//行動
-	int[] action = null;
 	//出力数
 	int numoutput = 0;
 	//出力
 	int[] output = null;
-	//状態遷移確率([この状態で][この行動をとると][この状態になる]確率)
-	double[][][] protransition = null;
+	//初期状態確率
+	double[] proinitial = null;
+	//状態遷移確率([この状態から][この状態になる]確率)
+	double[][] protransition = null;
 	//出力確率([この状態で][この出力を得る]確率)
 	double[][] prooutput = null;
 
@@ -30,35 +28,111 @@ public class TestHMM {
 //すべてにおいて、-1は不正
 	//現在の状態（不可視）
 	int curstatus = -1;
-	//現在の行動
-	int curaction = -1;
 	//現在の出力
 	int curoutput = -1;
 
 	//コンストラクタ
-	public TestHMM(int numstatus,int numaction,int numoutput){
+	public TestHMM(int numstatus,int numoutput){
+/*
 		this.numaction = numaction;
+*/
 		this.numoutput = numoutput;
 		this.numstatus = numstatus;
 		this.prooutput = new double[numstatus][numoutput];
+		this.proinitial = new double[numstatus];
 
 		for(int i=0;i<numstatus;i++){
+			//理由不十分の原理
+			this.proinitial[i] = 1/numstatus;
 			for(int j=0;j<numoutput;j++){
 				//理由不十分の原理
-				this.prooutput[i][j] = 1/numoutput;
+				this.prooutput[i][j] = (double)1/numoutput;
 			}
 		}
 
-		this.protransition = new double[numstatus][numaction][numstatus];
+		this.protransition = new double[numstatus][numstatus];
 		for(int i=0;i<numstatus;i++){
-			for(int j=0;j<numaction;j++){
-				for(int k=0;k<numstatus;k++){
-					//理由不十分の原理
-					this.protransition[i][j][k] = 1/numstatus;
-				}
+			for(int k=0;k<numstatus;k++){
+				//理由不十分の原理
+				this.protransition[i][k] = (double)1/numstatus;
 			}
 		}
 	}
+/* ****************************************************************************************************** */
+/*セッター、ゲッター                                                                                                                                                                                                                                                     */
+	//現状態のゲッター。本来不可視
+	public int getCurStatus(){
+		return this.curstatus;
+	}
+	//状態遷移確率のセッター
+	public TestHMM setProTransition(double[][] inputs){
+		//正規化
+		for(int i=0;i<this.numstatus;i++){
+			double temp = 0;
+			for(int j=0;j<this.numstatus;j++){
+				temp += this.protransition[i][j];
+			}
+			for(int j=0;j<this.numstatus;j++){
+				this.protransition[i][j] = inputs[i][j]/temp;
+			}
+		}
+		return this;
+	}
+
+	//初期状態確率のセッター
+	public TestHMM setProInitial(double[] inputs){
+		//正規化
+		double temp=0;
+		for(int i=0;i<this.numstatus;i++){
+			temp+=inputs[i];
+		}
+		for(int i=0;i<this.numstatus;i++){
+			this.proinitial[i] = inputs[i]/temp;
+		}
+		return this;
+	}
+/* ****************************************************************************************************** */
+	//HMMの初期化
+	/*
+	 * 初期状態確率に従って状態を選択
+	 */
+	public void initialize(){
+		double random = Math.random();
+		int idx = 0;
+		double temp = this.proinitial[idx];
+		while(random > temp){
+			idx++;
+			temp += this.proinitial[idx];
+		}
+		this.curstatus = idx;
+	}
+
+	//出力。
+	//現在の状態から、出力確率に従って出力する
+	public int output(){
+		double random = Math.random();
+		int idx = 0;
+		double temp = this.prooutput[curstatus][idx];
+		while(random > temp){
+			idx++;
+			temp += this.prooutput[curstatus][idx];
+		}
+	return idx;
+	}
+
+	//状態遷移。
+	//現在の状態から、状態遷移確率に従って状態遷移する
+	public void transition(){
+		double random = Math.random();
+		int idx = 0;
+		double temp = this.protransition[curstatus][idx];
+		while(random > temp){
+			idx++;
+			temp += this.protransition[curstatus][idx];
+		}
+		this.curstatus = idx;
+	}
+
 
 	//「散歩」「洗濯」「読書」が「晴れ」「晴れ」「雨」から出力したことを推定する
 
