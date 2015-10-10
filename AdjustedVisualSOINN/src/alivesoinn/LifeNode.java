8d57b47b7e426a911bbc1f31d0624f44c2e92af9
TestsuyaKomota <komotatit@gmail.com>
2015-10-11 00:14:38 +0900

    なんかした。

--- AdjustedVisualSOINN/src/alivesoinn/LifeNode.java
+++ AdjustedVisualSOINN/src/alivesoinn/LifeNode.java
@@ -3,5 +3,36 @@ package alivesoinn;
 import soinn.Node;
 
 public class LifeNode extends Node{
+	//ライフSOINNのノード。速度、加速度の概念を追加する
+
+	//フィールド
+	//速度
+	private double[] velocity = null;
+	//加速度
+	private double[] acceleration = null;
+
+	//コンストラクタ
+	public LifeNode(){
+		super();
+		this.velocity = new double[Node.dimension];
+		this.acceleration = new double[Node.dimension];
+	}
+
+	//getta-
+	public double[] getVelocity(){
+		return this.velocity;
+	}
+
+
+	//速度を結合重みに加算
+	public void move(){
+		double[] temp = new double[Node.dimension];
+		for(int i=0;i<Node.dimension;i++){
+			temp[i] = this.getSignal()[i] + this.getVelocity()[i];
+		}
+
+	}
+
+
 
 }
