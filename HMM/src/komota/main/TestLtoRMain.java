5904a2882f6b6cab40f443dd21a7c1140a8b15ae
TetsuyaKomota <komotatit@gmail.com>
2015-10-21 15:19:01 +0900

    コミットするほどでもない

--- HMM/src/komota/main/TestLtoRMain.java
+++ HMM/src/komota/main/TestLtoRMain.java
@@ -12,6 +12,7 @@ public class TestLtoRMain {
 		//あのプリントの例を再現
 
 		System.out.println("*******************************************************************************************");
+/*
 		LtoRHMM hmm = new LtoRHMM(4,3);
 		//状態遷移確率を設定
 		double[][] temptrans = new double[4][4];
@@ -53,8 +54,8 @@ public class TestLtoRMain {
 
 		int[] tempinputs = {0,0,1,2};
 
-//		System.out.println("Current likelihood:"+hmm.getHMMLikelihood(tempinputs));
-//		hmm.show();
+		//		System.out.println("Current likelihood:"+hmm.getHMMLikelihood(tempinputs));
+		//		hmm.show();
 		hmm.learnwithBaum_Welch(tempinputs);
 		hmm.show();
 		//学習したパラメータを用いて出力列を再現
@@ -66,7 +67,7 @@ public class TestLtoRMain {
 			if(hmm.getCurStatus() == hmm.numstatus-1)break;
 		}
 		System.out.println("");
-//		System.out.println("Next likelihood:"+hmm.getHMMLikelihood(tempinputs));
+		//		System.out.println("Next likelihood:"+hmm.getHMMLikelihood(tempinputs));
 
 		//複数の出力列からの学習のテスト
 		System.out.println("Test for learning from some outputs");
@@ -87,7 +88,7 @@ public class TestLtoRMain {
 		hmm2.learnwithBaum_Welch(tempa);
 		System.out.println("temp2");
 		hmm2.learnwithBaum_Welch(tempb);
-//		hmm2.show();
+		//		hmm2.show();
 		System.out.println("temp3");
 		hmm2.learnwithBaum_Welch(tempc);
 		System.out.println("temp4");
@@ -105,11 +106,11 @@ public class TestLtoRMain {
 			}
 			System.out.println("");
 		}
-//		hmm2.show();
+		//		hmm2.show();
 
-		LtoRHMM hmm3 = new LtoRHMM(5,4);
+		LtoRHMM hmm3 = new LtoRHMM(10,4);
 
-		int[] temps0 = {0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3};
+		int[] temps0 = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3};
 		int[] temps1 = {0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3};
 		int[] temps2 = {0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3};
 		int[] temps3 = {0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3};
@@ -156,6 +157,32 @@ public class TestLtoRMain {
 				System.out.print(hmm3.output()+" ");
 				hmm3.transition();
 				if(hmm3.getCurStatus() == hmm3.numstatus-1)break;
+			}
+			System.out.println("");
+		}
+*/
+
+		System.out.println("Test for limit of status");
+
+		int numstatus = 10;
+		int numoutput  = numstatus * 100;
+
+		LtoRHMM hmm4 = new LtoRHMM(numstatus,numoutput);
+
+		int[] templimitoutput = new int[numstatus*10];
+		for(int i=0;i<templimitoutput.length;i++){
+			templimitoutput[i] = i/5;
+		}
+		hmm4.learnwithBaum_Welch(templimitoutput);
+//		hmm4.show();
+		int count = 0;
+		while(count++ < 100){
+			hmm4.initialize();
+			System.out.print("Reproduction:");
+			while(true){
+				System.out.print(hmm4.output()+" ");
+				hmm4.transition();
+				if(hmm4.getCurStatus() == hmm4.numstatus-1)break;
 			}
 			System.out.println("");
 		}
