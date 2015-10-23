5630a5791f2a38637a8870d01b3e6d327d32de68
TetsuyaKomota <komotatit@gmail.com>
2015-10-23 16:40:48 +0900

    SerialLtoRHMMを作成。 出力確率の扱いを変えたいが、そのためにHMMの挙動を変える必要があることに気付く

---
+++ HMM/src/komota/test/TestSerialLtoRHMM.java
@@ -0,0 +1,17 @@
+package komota.test;
+
+import komota.main.LtoRHMM;
+
+public class TestSerialLtoRHMM extends LtoRHMM{
+
+	//出力確率を混合ガウスモデルで扱えるバージョン
+	//面倒だから既存の出力確率(prooutput)はそのまま残し、新たにprogaussoutputを作成
+
+	public TestSerialLtoRHMM(int numstatus, int numoutput) {
+		super(numstatus, numoutput);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	//
+
+}
