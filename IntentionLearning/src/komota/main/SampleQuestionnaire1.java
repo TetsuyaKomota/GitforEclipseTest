538108ffe644ecc1a5eadb322df14a28fa194dcd
TetsuyaKomota <komotatit@gmail.com>
2015-11-06 16:12:46 +0900

    アンケート作成中

--- IntentionLearning/src/komota/main/SampleQuestionnaire1.java
+++ IntentionLearning/src/komota/main/SampleQuestionnaire1.java
@@ -42,6 +42,39 @@ public class SampleQuestionnaire1 extends MyFrame{
 		this.patterns[2].setPattern(1,5,1);
 		this.patterns[2].setPattern(10,15,2);
 		this.patterns[2].setPattern(9,12,3);
+		this.patterns[3] = new Pattern("これらのパネルを動かして、下に記された\"タスク\"を達成してください(2で進む)","タスク：赤　を　青　の左に動かす ←これ");
+		this.patterns[3].setPattern(1,5,1);
+		this.patterns[3].setPattern(10,15,2);
+		this.patterns[3].setPattern(9,12,3);
+		this.patterns[4] = new Pattern("色付きの\"オブジェクト\"を、白色の\"空きパネル\"に移動することができます。赤いオブジェクトをクリックしてください(2で進む)","タスク：赤　を　青　の左に動かす");
+		this.patterns[4].setPattern(1,5,1);
+		this.patterns[4].setPattern(10,15,2);
+		this.patterns[4].setPattern(9,12,3);
+		this.patterns[5] = new Pattern("青のオブジェクトのすぐ左の空きパネルをクリックしてください(2で進む)","タスク：赤　を　青　の左に動かす");
+		this.patterns[5].setPattern(1,5,1);
+		this.patterns[5].setPattern(10,15,2);
+		this.patterns[5].setPattern(9,12,3);
+		this.patterns[5].setAnswer(1, 5, -1, -1);
+		this.patterns[6] = new Pattern("\"オブジェクト\"と\"空きパネル\"が選択されている状態になったら、スペースキーを押してください(2で進む)","タスク：赤　を　青　の左に動かす");
+		this.patterns[6].setPattern(1,5,1);
+		this.patterns[6].setPattern(10,15,2);
+		this.patterns[6].setPattern(9,12,3);
+		this.patterns[6].setAnswer(1, 5, 10, 14);
+		this.patterns[7] = new Pattern("これで\"オブジェクト\"が移動できました。(2で進む)","タスク：赤　を　青　の左に動かす");
+		this.patterns[7].setPattern(10,14,1);
+		this.patterns[7].setPattern(10,15,2);
+		this.patterns[7].setPattern(9,12,3);
+		this.patterns[7].setAnswer(-1,-1,-1,-1);
+		this.patterns[8] = new Pattern("\"タスク：赤　を　青　の左に動かす\" が達成できたので、gを入力してください。(画面上では何も起こりません)(2で進む)","タスク：赤　を　青　の左に動かす");
+		this.patterns[8].setPattern(10,14,1);
+		this.patterns[8].setPattern(10,15,2);
+		this.patterns[8].setPattern(9,12,3);
+		this.patterns[8].setAnswer(-1,-1,-1,-1);
+		this.patterns[9] = new Pattern("では、次のタスクをやってみてください。完了したらgを一回押した後、2で進んでください(2で進む)","タスク：赤　を　黄　の上に動かす");
+		this.patterns[9].setPattern(12,6,1);
+		this.patterns[9].setPattern(18,17,2);
+		this.patterns[9].setPattern(13,1,3);
+		this.patterns[9].setAnswer(-1,-1,-1,-1);
 		/* ************************************************************************************************************************************* */
 
 		initialize();
@@ -59,7 +92,8 @@ public class SampleQuestionnaire1 extends MyFrame{
 				this.panels[i][j].setStatus(this.patterns[this.currentpattern].getPattern()[i][j]);
 			}
 		}
-
+		this.patterns[this.currentpattern].answer();
+		this.pw.println("page ,"+this.currentpattern);
 		//this.outputStart();
 	}
 	@Override
@@ -144,6 +178,23 @@ public class SampleQuestionnaire1 extends MyFrame{
 			this.ans[1] = ansr;
 			this.secondans[0] = secansg;
 			this.secondans[1] = secansr;
+		}
+		void answer(){
+			if(this.ans[0] != -1 && this.ans[1] != -1){
+				SampleQuestionnaire1.this.selected = ans;
+			}
+			else{
+				SampleQuestionnaire1.this.selected[0] = -1;
+				SampleQuestionnaire1.this.selected[1] = -1;
+			}
+			if(this.secondans[0] != -1 && this.secondans[1] != -1){
+				SampleQuestionnaire1.this.secondselected = secondans;
+			}
+			else{
+				SampleQuestionnaire1.this.secondselected[0] = -1;
+				SampleQuestionnaire1.this.secondselected[1] = -1;
+			}
+
 		}
 	}
 
