1ec4e4591378da8aa179410001f2b85fa9d562df
TetsuyaKomota <komotatit@gmail.com>
2015-11-17 13:47:58 +0900

    座標系を試す2_1s系クラスを作成(まだ001sコピペしただけ)

--- IntentionLearning/src/komota/coordinate/MyCoordinate_TEST.java
+++ IntentionLearning/src/komota/coordinate/MyCoordinate_TEST.java
@@ -2,4 +2,28 @@ package komota.coordinate;
 
 public class MyCoordinate_TEST {
 
+	//座標変換を行うクラス。PR一つ一つに異なるMyCoordinateを与えることによって、各PRごとに座標変換を割り当てることができる
+
+	//定数
+	//フィールド
+
+	//変換メソッド。座標系ごとに、変換に必要なベクトルの種類や数が違うため、ベクトルの配列を引数に渡す
+	//ルールとして、引数に渡すベクトル配列は、必ず変換元のベクトルを第一引数にする
+	public double[] convert(double[][] inputs){
+		double[] output = new double[2];
+		//ここでは単純に恒等写像。オーバーライドして使う
+		output[0] = inputs[0][0];
+		output[1] = inputs[0][1];
+		return output;
+	}
+	//逆変換メソッド。座標系ごとに、変換に必要なベクトルの種類や数が違うため、ベクトルの配列を引数に渡す
+	//ルールとして、引数に渡すベクトル配列は、必ず変換元のベクトルを第一引数にする
+	public double[] inverseConvert(double[][] inputs){
+		double[] output = new double[2];
+		//ここでは単純に恒等写像。オーバーライドして使う
+		output[0] = inputs[0][0];
+		output[1] = inputs[0][1];
+		return output;
+	}
+
 }
---
+++ IntentionLearning/src/komota/main/SampleTask_2_1s.java
@@ -0,0 +1,140 @@
+package komota.main;
+
+import komota.pr.test.PR_2_1;
+
+public class SampleTask_2_1s extends MySerialFrame{
+
+	public static void main(String[] args){
+		SampleTask_2_1s task = new SampleTask_2_1s();
+	}
+
+	/* ************************************************************************************************************** */
+	/* ************************************************************************************************************** */
+	/*
+	 * 座標系を扱うテスト
+	 */
+	/* ************************************************************************************************************** */
+	/* ************************************************************************************************************** */
+
+	//解析クラス
+//	TestPatternRecognition tpr;
+	PR_2_1 tpr1;
+
+	//コンストラクタ
+	public SampleTask_2_1s(){
+		super();
+//		this.tpr = new TestPatternRecognition();
+		this.tasktitle = "Move the RED to left of the BLUE.";
+		setOutputFile("test4.txt");
+		initialize();
+	}
+
+	@Override
+	public void initialize(){
+
+		for(int i=0;i<this.panels.length;i++){
+			for(int j=0;j<this.panels[0].length;j++){
+				this.panels[i][j].setStatus(0);
+			}
+		}
+
+		this.panels[(int)(Math.random() * MyFrame.NUMBEROFPANEL)][(int)(Math.random() * MyFrame.NUMBEROFPANEL)].setStatus(1);
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+				this.panels[temp1][temp2].setStatus(2);
+				break;
+			}
+		}
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+				this.panels[temp1][temp2].setStatus(3);
+				break;
+			}
+		}
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+				this.panels[temp1][temp2].setStatus(4);
+				break;
+			}
+		}
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+				this.panels[temp1][temp2].setStatus(5);
+				break;
+			}
+		}
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+				this.panels[temp1][temp2].setStatus(6);
+				break;
+			}
+		}
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+//				this.panels[temp1][temp2].setStatus(7);
+				break;
+			}
+		}
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+//				this.panels[temp1][temp2].setStatus(8);
+				break;
+			}
+		}
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+//				this.panels[temp1][temp2].setStatus(9);
+				break;
+			}
+		}
+
+		this.outputStart();
+	}
+	@Override
+	public void functionPlugin1(){
+		System.out.println("additional function.");
+		this.tpr1 = new PR_2_1(9,this.file_name);
+//		this.tpr.testConvert(this);
+	}
+	@Override
+	public void functionPlugin2(){
+		System.out.println("show.");
+//		this.tpr1.show();
+		this.tpr1.learnfromLog();
+//		this.tpr1.showReference();
+	}
+	@Override
+	public void functionPlugin3(){
+		System.out.println("reproduction.");
+		this.tpr1.reproduction(this);
+	}
+	@Override
+	public void functionPlugin4(){
+		System.out.println("bye");
+		this.tpr1.close();
+	}
+	/*
+	 *
+	 * １．出力先ファイル名をfile_nameに代入
+	 * ２．タスク名をtasktitleに代入
+	 * ３．initializeをオーバーライド。最終行にoutputStart()
+	 *
+	 */
+
+}
