c651393907b0160fb55e2a83f1bb8f57b4d0b725
Komota <komotatit@gmail.com>
2015-11-19 15:23:52 +0900

    リファクタリングなりなんなり。 2_1シリーズが完成したっぽいから、これから大幅にリファクタリングする

---
+++ IntentionLearning/src/komota/coordinate/Coordinate2_1_ID.java
@@ -0,0 +1,21 @@
+package komota.coordinate;
+
+public class Coordinate2_1_ID extends MyCoordinate_TEST{
+
+	//変換、逆変換ともにただの恒等写像
+	@Override
+	public double[] convert(double[][] inputs){
+		double[] output = new double[2];
+		output[0] = inputs[0][0];
+		output[1] = inputs[0][1];
+		return output;
+	}
+	@Override
+	public double[] inverseConvert(double[][] inputs){
+		double[] output = new double[2];
+		output[0] = inputs[0][0];
+		output[1] = inputs[0][1];
+		return output;
+	}
+
+}
---
+++ IntentionLearning/src/komota/pr/test/PR_2_2_LT.java
@@ -0,0 +1,12 @@
+package komota.pr.test;
+
+import komota.coordinate.Coordinate2_1_LT;
+
+public class PR_2_2_LT extends PR_2_1_ID{
+
+	public PR_2_2_LT(int numref, String filename) {
+		super(numref, filename);
+		// TODO 自動生成されたコンストラクター・スタブ
+		this.coordinate = new Coordinate2_1_LT();
+	}
+}
