7e00e4d8c484b45b96b6b5d34a930c231a556b47
Komota <komotatit@gmail.com>
2015-11-19 14:54:09 +0900

    ランドマークトラジェクタ変換を実装。ほぼうまくいってるっぽいけど、ちょっと違う。 たぶんCoordinate_2_1で±をちょっと間違えてるっぽい。

--- IntentionLearning/src/komota/main/SampleTask_2_1s.java
+++ IntentionLearning/src/komota/main/SampleTask_2_1s.java
@@ -59,7 +59,7 @@ public class SampleTask_2_1s extends MySerialFrame{
 			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
 			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
 			if(this.panels[temp1][temp2].getStatus() == 0){
-				this.panels[temp1][temp2].setStatus(4);
+//				this.panels[temp1][temp2].setStatus(4);
 				break;
 			}
 		}
@@ -67,7 +67,7 @@ public class SampleTask_2_1s extends MySerialFrame{
 			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
 			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
 			if(this.panels[temp1][temp2].getStatus() == 0){
-				this.panels[temp1][temp2].setStatus(5);
+//				this.panels[temp1][temp2].setStatus(5);
 				break;
 			}
 		}
@@ -75,7 +75,7 @@ public class SampleTask_2_1s extends MySerialFrame{
 			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
 			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
 			if(this.panels[temp1][temp2].getStatus() == 0){
-				this.panels[temp1][temp2].setStatus(6);
+//				this.panels[temp1][temp2].setStatus(6);
 				break;
 			}
 		}
---
+++ IntentionLearning/src/komota/test/Gomi.java
@@ -0,0 +1,10 @@
+package komota.test;
+
+public class Gomi {
+
+	public static void main(String[] args){
+		double bunbo = Math.sqrt(914);
+		System.out.println(108/bunbo + "," + -110/bunbo);
+	}
+
+}
