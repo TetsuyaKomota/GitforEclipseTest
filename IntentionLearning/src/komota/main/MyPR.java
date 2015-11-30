9909e55f71e639cb8eee2592761d99dd8723e757
TetsuyaKomota <komotatit@gmail.com>
2015-11-30 23:39:46 +0900

    評価ポイントを平均誤差に変更。5_1系を005にリファクタリング

--- IntentionLearning/src/komota/main/MyPR.java
+++ IntentionLearning/src/komota/main/MyPR.java
@@ -166,7 +166,7 @@ public class MyPR {
 				//再現したsecondselectedとのずれを求める
 				double diserror = Math.sqrt((frame.secondselected[0]-truepoint[0])*(frame.secondselected[0]-truepoint[0]) + (frame.secondselected[1]-truepoint[1])*(frame.secondselected[1]-truepoint[1]));
 				System.out.println("step:"+t+"  true:"+truepoint[0]+","+truepoint[1]+" select:"+frame.secondselected[0]+","+frame.secondselected[1]+" error:"+diserror);
-				diserror = 1/(1+diserror);
+				//diserror = 1/(1+diserror);
 				//outputの更新。outputはdiserrorの平均値にする
 				output = (count/(count+1))*output + (1/(count+1))*diserror;
 			}
