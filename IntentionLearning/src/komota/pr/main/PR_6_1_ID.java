28558ce02dc099274df512558afeeb7d96db2c1d
TetsuyaKomota <komotatit@gmail.com>
2015-12-09 15:12:35 +0900

    タスク的視野試作用の6_1系を作成。ログファイルはtest6.txtに変更

---
+++ IntentionLearning/src/komota/pr/main/PR_6_1_ID.java
@@ -0,0 +1,12 @@
+package komota.pr.main;
+
+import komota.coordinate.Coordinate_ID;
+
+public class PR_6_1_ID extends PR_6_1{
+
+	public PR_6_1_ID(int numref, String filename) {
+		super(numref, filename);
+		// TODO 自動生成されたコンストラクター・スタブ
+		this.coordinate = new Coordinate_ID();
+	}
+}
