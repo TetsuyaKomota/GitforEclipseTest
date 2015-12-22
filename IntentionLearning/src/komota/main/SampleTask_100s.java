ea6bc1a6e7c71efca434d75b0acd7f42cad9f7f6
TetsuyaKomota <komotatit@gmail.com>
2015-12-22 23:33:30 +0900

    全クラスのブラッシュアップ版001シリーズを作成開始。 完成した場合、これ以降は001シリーズをもとに修正、継承していく

---
+++ IntentionLearning/src/komota/main/SampleTask_100s.java
@@ -0,0 +1,173 @@
+package komota.main;
+
+import komota.pr.main.PR_7_1_GL;
+import komota.pr.main.PR_7_1_ID;
+import komota.pr.main.PR_7_1_LT;
+
+public class SampleTask_100s extends MySerialFrame{
+
+	public static void main(String[] args){
+		SampleTask_100s task = new SampleTask_100s();
+	}
+
+	/* ************************************************************************************************************** */
+	/* ************************************************************************************************************** */
+	/*
+	 * ReferencePoint_Gaussのテスト
+	 */
+	/* ************************************************************************************************************** */
+	/* ************************************************************************************************************** */
+
+	//解析クラス
+//	TestPatternRecognition tpr;
+	PR_7_1_LT pr_LT;
+	PR_7_1_ID pr_ID;
+	PR_7_1_GL pr_GL;
+
+	//コンストラクタ
+	public SampleTask_100s(){
+		super();
+//		this.tpr = new TestPatternRecognition();
+		this.tasktitle = "動作名";
+		setOutputFile("logdata.txt");
+		initialize();
+	}
+
+	@Override
+	public void initialize(){
+
+		for(int i=0;i<this.panels.length;i++){
+			for(int j=0;j<this.panels[0].length;j++){
+				this.panels[i][j].setStatus(0);
+			}
+		}
+
+		/*
+		 * 環境中の物体配置は、以下のように定める
+		 * 1. トラジェクタの位置をランダムに決定する
+		 * 2. 物体2(青色のオブジェクト)の位置を、トラジェクタの位置以外でランダムに決定する
+		 * 3. 物体3(黄色のオブジェクト)の位置を、物体2の上下左右にそれぞれ40マス離れた位置のうち、可能な位置からランダムに決定する
+		 * 4. 物体4(緑色のオブジェクト)の位置を、現在物体が配置されていない位置からランダムに決定する
+		 * 5. 物体5(橙色のオブジェクト)の位置を、現在物体が配置されていない位置からランダムに決定する
+		 *
+		 * つまり環境中には1つのトラジェクタと4つのオブジェクトが配置され、そのうち必ず上下左右40マスに隣り合う青色と黄色以外は独立にランダムで場所が決定する
+		 */
+
+
+		this.panels[(int)(Math.random() * MyFrame.NUMBEROFPANEL)][(int)(Math.random() * MyFrame.NUMBEROFPANEL)].setStatus(1);
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+				this.panels[temp1][temp2].setStatus(2);
+				while(true){
+					double temp3 = Math.random();
+					if(temp3<0.25&&temp1>50&&this.panels[temp1-40][temp2].getStatus() == 0){
+						this.panels[temp1-40][temp2].setStatus(3);
+						break;
+					}else if(temp3<0.5&&temp2>50&&this.panels[temp1][temp2-40].getStatus() == 0){
+						this.panels[temp1][temp2-40].setStatus(3);
+						break;
+					}else if(temp3<0.75&&temp1<MyFrame.NUMBEROFPANEL-30&&this.panels[temp1+40][temp2].getStatus() == 0){
+						this.panels[temp1+40][temp2].setStatus(3);
+						break;
+					}else if(temp3<1.1&&temp2<MyFrame.NUMBEROFPANEL-30&&this.panels[temp1][temp2+40].getStatus() == 0){
+						this.panels[temp1][temp2+40].setStatus(3);
+						break;
+					}
+				}
+				break;
+			}
+		}
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+				this.panels[temp1][temp2].setStatus(4);
+				break;
+			}
+		}
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+				this.panels[temp1][temp2].setStatus(5);
+				break;
+			}
+		}
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+//				this.panels[temp1][temp2].setStatus(6);
+				break;
+			}
+		}
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+//				this.panels[temp1][temp2].setStatus(7);
+				break;
+			}
+		}
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+//				this.panels[temp1][temp2].setStatus(8);
+				break;
+			}
+		}
+		while(true){
+			int temp1 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			int temp2 = (int)(Math.random() * MyFrame.NUMBEROFPANEL);
+			if(this.panels[temp1][temp2].getStatus() == 0){
+//				this.panels[temp1][temp2].setStatus(9);
+				break;
+			}
+		}
+
+		this.outputStart();
+	}
+	@Override
+	public void functionPlugin1(){
+		System.out.println("各パターン認識クラスのインスタンスを生成");
+		this.pr_LT = new PR_7_1_LT(9,this.file_name);
+		this.pr_ID = new PR_7_1_ID(9,this.file_name);
+		this.pr_GL = new PR_7_1_GL(9,this.file_name);
+	}
+	@Override
+	public void functionPlugin2(){
+		System.out.println("ログデータから学習");
+		this.pr_LT.learnfromLog();
+		this.pr_ID.learnfromLog();
+		this.pr_GL.learnfromLog();
+	}
+	@Override
+	public void functionPlugin3(){
+		System.out.println("学習結果から、動作を再現");
+		if(this.pr_ID.getMaxLikelihood() >= this.pr_LT.getMaxLikelihood() && this.pr_ID.getMaxLikelihood() >= this.pr_GL.getMaxLikelihood()){
+			this.pr_ID.reproduction(this);
+		}
+		else if(this.pr_LT.getMaxLikelihood() >= this.pr_ID.getMaxLikelihood() && this.pr_LT.getMaxLikelihood() >= this.pr_GL.getMaxLikelihood()){
+			this.pr_LT.reproduction(this);
+		}
+		else{
+			this.pr_GL.reproduction(this);
+		}
+	}
+	@Override
+	public void functionPlugin4(){
+		System.out.println("再現動作の評価値を計算");
+		if(this.pr_ID.getMaxLikelihood() >= this.pr_LT.getMaxLikelihood() && this.pr_ID.getMaxLikelihood() >= this.pr_GL.getMaxLikelihood()){
+			System.out.println("evaluation point:"+this.pr_ID.evaluate(this));
+		}
+		else if(this.pr_LT.getMaxLikelihood() >= this.pr_ID.getMaxLikelihood() && this.pr_LT.getMaxLikelihood() >= this.pr_GL.getMaxLikelihood()){
+			System.out.println("evaluation point:"+this.pr_LT.evaluate(this));
+		}
+		else{
+			System.out.println("evaluation point:"+this.pr_GL.evaluate(this));
+		}
+	}
+}
