b674617aab559080566b35763aa68be6132db735
TetsuyaKomota <komotatit@gmail.com>
2015-12-23 11:55:48 +0900

    changeログとstatusログをとらないようにした。 使用データ量と誤差の関係を計算する際、採用するデータを与えた順にしてるのまずくない？という指摘があったので、 乱択できるようにする

--- IntentionLearning/src/komota/main/MyFrame.java
+++ IntentionLearning/src/komota/main/MyFrame.java
@@ -283,6 +283,8 @@ public class MyFrame extends JFrame{
 					int temp = MyFrame.this.panels[selected[0]][selected[1]].getStatus();
 					MyFrame.this.panels[selected[0]][selected[1]].setStatus(MyFrame.this.panels[secondselected[0]][secondselected[1]].getStatus());
 					MyFrame.this.panels[secondselected[0]][secondselected[1]].setStatus(temp);
+					//下記、changeログとstatusログは現状使用していないため、使用するまでコメントアウト
+/*
 					MyFrame.this.pw.print("change,");
 					for(int i=0;i<MyFrame.this.panels.length;i++){
 						for(int j=0;j<MyFrame.this.panels[0].length;j++){
@@ -301,7 +303,8 @@ public class MyFrame extends JFrame{
 						}
 					}
 					pw.println();
-
+*/
+/*
 					MyFrame.this.pw.print("status,");
 					for(int i=0;i<MyFrame.this.panels.length;i++){
 						for(int j=0;j<MyFrame.this.panels[0].length;j++){
@@ -312,7 +315,7 @@ public class MyFrame extends JFrame{
 						}
 					}
 					pw.println();
-
+*/
 					MyFrame.this.selected[0] = -1;
 					MyFrame.this.selected[1] = -1;
 					MyFrame.this.secondselected[0] = -1;
