4820db725eb0642f54be837ace528756cdb681ce
TetsuyaKomota <komotatit@gmail.com>
2016-01-07 14:33:09 +0900

    各動作の教示データ数を30に統一。csvファイル作り直しねー。

---
+++ IntentionLearning/src/komota/test/MyCounter.java
@@ -0,0 +1,51 @@
+package komota.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MyCounter {
+
+	//ログデータ数を数えたりする用の雑なやつ
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		MyCounter counter = new MyCounter();
+
+		try {
+			System.out.println("RtB:"+counter.countGoal("log_RIGHT_TO_BLUE.txt"));
+			System.out.println("NbO:"+counter.countGoal("log_NEAR_BY_ORANGE.txt"));
+			System.out.println("MtS:"+counter.countGoal("log_MAKE_THE_SIGNAL.txt"));
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+	}
+
+	//引数で与えたデータの教示回数（goalの数）を数える
+	public int countGoal(String filename) throws IOException{
+		int output = 0;
+
+		File file = new File("log/"+filename);
+		BufferedReader br = null;
+
+		br = new BufferedReader(new FileReader(file));
+
+		String line = br.readLine();
+
+		while(line != null){
+			if(line.split(",")[0].equals("goal  ")){
+				output++;
+			}
+			line = br.readLine();
+		}
+		br.close();
+
+		return output;
+	}
+
+}
