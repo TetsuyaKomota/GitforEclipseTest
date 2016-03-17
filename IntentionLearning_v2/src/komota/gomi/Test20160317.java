b515415234ac871531e8acff4c5b6ecb61f5884a
TetsuyaKomota <komotatit@gmail.com>
2016-03-17 17:22:50 +0900

    クラスパスsoinnじゃなくてAdjustedVisualSOINNにした． SOINNにベクトル投げてみたけどうまくいかない．不要ノード削除で全部消える理由としては， （正規の）データがすべてまったく同じベクトルだからかもしれない．まったく同じベクトルをいくつ投げてもノードは2つ以上生まれないのかもしれない．ちょっと試してみよう．

--- IntentionLearning_v2/src/komota/gomi/Test20160317.java
+++ IntentionLearning_v2/src/komota/gomi/Test20160317.java
@@ -2,6 +2,7 @@ package komota.gomi;
 
 import komota.lib.MyIO;
 import komota.lib.MyMatrix;
+import komota.supers.AdjustedVisualSOINN;
 import soinn.SOINN;
 
 public class Test20160317 {
@@ -11,8 +12,7 @@ public class Test20160317 {
 	 */
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		SOINN soinn = new SOINN(0,0,0);
-		MyMatrix mat1 = new MyMatrix(3);
+		MyMatrix mat1 = new MyMatrix(11);
 		MyIO io = new MyIO();
 		io.readFile("test.txt");
 		mat1 = io.readMatrix(1);
@@ -22,6 +22,32 @@ public class Test20160317 {
 			System.out.print(vec1[i]+" ");
 		}
 		System.out.println();
+
+		//SOINNを試す
+		SOINN soinn = new SOINN(121,1000,5);
+		AdjustedVisualSOINN frame = new AdjustedVisualSOINN(soinn,"VisualSOINN",50,50,50,true);
+
+		io.readFile("20160317/testoutputmatrix.txt");
+
+		for(int idx=0;idx<70;idx++){
+			mat1 = io.readMatrix(idx);
+			if(mat1 != null){
+				vec1 = mat1.vectorize();
+				soinn.inputSignal(vec1);
+				soinn.classify();
+				System.out.println(idx+"番目のデータを入れたときは"+ soinn.getClassNum()+"個だよ");
+			}
+		}
+		soinn.removeUnnecessaryNode();
+		soinn.classify();
+
+
+		if(soinn.getClassNum() == 1){
+			System.out.println("やったね！");
+		}
+		else{
+			System.out.println("ふぇぇ．．"+soinn.getClassNum()+"個だよ...");
+		}
 	}
 
 }
