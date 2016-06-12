f128498e45ec190c2f9c0db5445d6483f2afbd79
komot <komot@DESKTOP-C5VE50F>
2016-06-12 22:54:12 +0900

    J4KSDKお試し中． わかったことはEvernoteに書いてあるから見ておいてください

--- IntentionLearning_v2/src/komota/gomi/Test20160612.java
+++ IntentionLearning_v2/src/komota/gomi/Test20160612.java
@@ -18,18 +18,38 @@ public class Test20160612 extends J4KSDK{
 
 		Test20160612 kinect = new Test20160612();
 		kinect.start(J4KSDK.COLOR|J4KSDK.INFRARED|J4KSDK.DEPTH|J4KSDK.SKELETON);
+		while(true){
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+			System.out.println("ひでぶ");
+		}
 	}
 
 	@Override
 	public void onColorFrameEvent(byte[] arg0) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Color を受け取ったよ！");
+		System.out.print("Color を受け取ったよ！");
+		for(int i=0;i<arg0.length;i++){
+			System.out.print(",  "+arg0[i]);
+		}
+		System.out.println();
 	}
 
 	@Override
 	public void onDepthFrameEvent(short[] arg0, byte[] arg1, float[] arg2, float[] arg3) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Depth を受け取ったよ！");
+		System.out.print("Depth を受け取ったよ！arg0:"+arg0.length);
+/*
+  		for(int i=0;i<arg0.length;i++){
+
+			System.out.print(",  "+arg0[i]);
+		}
+		System.out.println();
+*/
 	}
 
 	@Override
