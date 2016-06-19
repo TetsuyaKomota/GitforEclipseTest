63471be9fbc292a5d87f5f018ec163ccd7fe3b56
komot <komot@DESKTOP-C5VE50F>
2016-06-20 00:24:06 +0900

    卒論条件のデータセットジェネレータ作った

--- IntentionLearning_v2/src/komota/lib/DataSetGenerator_v2.java
+++ IntentionLearning_v2/src/komota/lib/DataSetGenerator_v2.java
@@ -49,6 +49,21 @@ public class DataSetGenerator_v2 extends DataSetGenerator{
 		return next;
 	}
 
+	//卒論結果との比較実験をやる際に同条件（赤の位置のみにガウス誤差）を作るためのジェネレーター
+	public MyMatrix teach_prev(){
+		//grand truth と誤差によって現環境からの目標状態に動かす
+		//適切な（環境範囲内にすべて収まる）結果でない場合は null を返す
+		MyMatrix next;
+		MyMatrix current = this.getStatusforMatrix();
+		next = grtrh.mult(current);
+		//誤差を付与する
+
+		next.setData(1, 0, next.getData(1,0)+this.nextError(variance));
+		next.setData(2, 0, next.getData(2,0)+this.nextError(variance));
+
+		return next;
+	}
+
 	@Override
 	public void functionPlugin1(){
 		System.out.println("データジェネレート2！");
@@ -56,6 +71,33 @@ public class DataSetGenerator_v2 extends DataSetGenerator{
 		while(count < this.numberofdataset){
 			this.initialize();
 			MyMatrix next = this.teach();
+			if(next != null){
+				this.setStatusforMatrix(next);
+				this.pushSPACE();
+				this.pushGoal();
+				count++;
+			}
+			else{
+				System.out.println("だめだ！");
+			}
+		}
+		System.out.println("データジェネレート完了！");
+		try {
+			System.out.println("logdata.txtには"+this.countGoal("logdata.txt")+"個のデータが存在します");
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		this.getMyIO().close();
+
+	}
+	@Override
+	public void functionPlugin2(){
+		System.out.println("データジェネレート2_prev！");
+		int count = 0;
+		while(count < this.numberofdataset){
+			this.initialize();
+			MyMatrix next = this.teach_prev();
 			if(next != null){
 				this.setStatusforMatrix(next);
 				this.pushSPACE();
